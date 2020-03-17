93f1c6f72df8090c733870ddeb51275b843621f3
neethu <msneethu2989@gmail.com>
2020-03-17 14:32:36 -0400

    Add state for day

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,6 @@
-import React from "react";
-
+import React, {useState} from "react";
 import "components/Application.scss";
-import DayList from "components/DayList.js"
+import DayList from "components/DayList.js";
 
 const days = [
   {
@@ -23,6 +22,7 @@ const days = [
 
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -32,10 +32,10 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <DayList
+        <DayList 
           days={days}
-          day={"Monday"}
-          setDay={day => console.log(day)}
+          day={day}
+          setDay={setDay}
         />
         <nav className="sidebar__menu"></nav>
         <img
