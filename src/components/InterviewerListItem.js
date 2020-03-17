a4a255f17a71de7c78eca9b31b0fe48740baa8fc
neethu <msneethu2989@gmail.com>
2020-03-17 19:13:42 -0400

    Add InterviewerList component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/InterviewerListItem.scss';
+import classNames from 'classnames';
+
+export default function interviewListItem(props) {
+  console.log("props",props);
+
+  const interviewListClass = classNames("interviewers__item",
+    {
+      "interviewers__item--selected": props.selected,
+      "selected":props.selected
+    }
+  );
+
+  const interviewImgClass = classNames("interviewers__item-image");
+
+  return (
+    <li className={interviewListClass} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className={interviewImgClass}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  );
+};
+
