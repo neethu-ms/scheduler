b6b2686f5ff8b51b125b57d20835caaae544e4c6
neethu <msneethu2989@gmail.com>
2020-03-18 08:42:08 -0400

    Add stories for Confirm, Status and Error

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,9 @@ import Appointment from 'components/Appointment';
 import  Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
 
 storiesOf("Button", module)
   .addParameters({
@@ -140,3 +143,6 @@ storiesOf("Button", module)
         .add("Header",() => <Header time="12pm" />)
         .add("Empty",() => <Empty onAdd={action("onAdd")} />)
         .add("Show",() => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={(event) => action("onEdit")} onDelete={(event) => action("OnDelete")}/>)
+        .add("Confirm",() => <Confirm message="Delete the appointment" onConfirm={(event) => action("onConfirm")} onCancel={(event) => action("onCancel")}/>)
+        .add("Status",() => <Status message="Deleting"/>)
+        .add("Error",() => <Error message="Could not delete appointment." onClose={(event) => action("onClose")}/>)
