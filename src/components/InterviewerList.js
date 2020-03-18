0a89bc7ea230b60a90213fc0bf49697cd46b3812
neethu <msneethu2989@gmail.com>
2020-03-17 21:17:23 -0400

    Refactor code

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -25,7 +25,3 @@ export default function InterviewList(props) {
   );
 };
 
-{/* <section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"></ul>
-</section> */}
