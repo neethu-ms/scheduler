5f3687e7a85ed9954e2b7d69248a2b8b1691b1e4
neethu <msneethu2989@gmail.com>
2020-03-18 15:37:15 -0400

    Add Form component

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
+export default function(props){
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = function(){
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = function(){
+    reset();
+    props.onCancel();
+  }
+  return(
+    <main className="appointment__card appointment__card--create">
+    <section className="appointment__card-left">
+      <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+        <input onChange={(evt) => setName(evt.target.value)}
+          className="appointment__create-input text--semi-bold"
+          value={name}
+          type="text"
+          placeholder="Enter Student Name"
+          /*
+            This must be a controlled component
+          */
+        />
+      </form>
+      <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={(interviewerId) => {
+                setInterviewer(interviewerId)
+        }} />
+    </section>
+    <section className="appointment__card-right">
+      <section className="appointment__actions">
+        <Button danger onClick={() => cancel()}>Cancel</Button>
+        <Button confirm onClick={props.onSave(props.name,props.interviewer)}>Save</Button>
+      </section>
+    </section>
+  </main>
+  );
+}
