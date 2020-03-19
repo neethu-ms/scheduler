9445117b0ebd8ec06c2e43f1967e81001f3910b2
neethu <msneethu2989@gmail.com>
2020-03-19 19:50:41 -0400

    Refactor code

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,13 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import './styles.scss';
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
-export default function Appointment(props){
-   return(
-      
-   <article className="appointment">
-      <Header time={props.time}/>
-      {props.interview?<Show interviewer = {props.interview.interviewer} student={props.interview.student}/> : <Empty onAdd={() => console.log("OnAdd")}/>}
-   </article>
+export default function Appointment(props) {
+   return (
+      <article className="appointment">
+         <Header time={props.time} />
+         {props.interview ? <Show interviewer={props.interview.interviewer} student={props.interview.student} /> : <Empty onAdd={() => console.log("OnAdd")} />}
+      </article>
    );
 }
