fceb651fdfabeb67c9979cb25cf106f4adb4b7ae
neethu <msneethu2989@gmail.com>
2020-03-19 12:09:52 -0400

    Add selectors for getting appointments for a day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+export function getAppointmentsForDay(state, day) {
+
+  const selectedDay= state.days.filter((eachDay) => eachDay.name === day)[0];
+  const appointments = [];
+  if (selectedDay) {
+    for (let appointment of selectedDay["appointments"]) {
+      if (state.appointments[appointment]["id"] === appointment) {
+        appointments.push(state.appointments[appointment])
+      }
+    }
+  }
+  return appointments;
+};
