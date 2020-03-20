207300ef1185e1685db6b37154f7ebfe1baf9f8b
neethu <msneethu2989@gmail.com>
2020-03-20 19:05:10 -0400

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import DayListItem from "./components/DayListItem.js";
-import InterviewerList from "./components/InterviewerList.js"
-
+import InterviewerList from "./components/InterviewerList.js";
 import "index.scss";
-
 import Application from "components/Application";
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+ReactDOM.render(<Application key={"a"+1}/>, document.getElementById("root"));
