9234bc98fa52720b579a1071e53a9d60a47ed116
neethu <msneethu2989@gmail.com>
2020-03-20 17:26:32 -0400

    Add method for getting the interviewers of a day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -20,12 +20,27 @@ export function getInterview(state, interview){
       interviewer : {}
     };
   }
-
    if(!interviewers || !interview){
      return processedInterview;
    }
- 
-   processedInterview.interviewer = interviewers[interview.interviewer];
+    processedInterview.interviewer = interviewers[interview.interviewer];
    return processedInterview;
+};
 
+export function getInterviewersForDay (state, day) {
+  console.log("state",state);
+  console.log("day",day);
+  const selectedDay = state.days.filter((eachDay) => eachDay.name === day)[0];
+  console.log("selected day:",selectedDay);
+  const interviewers = [];
+  if (selectedDay) {
+    for (let appointment of selectedDay["appointments"]) {
+      if (state.appointments[appointment]["id"] === appointment) {
+        if(state.appointments[appointment].interview){
+          interviewers.push(state.appointments[appointment].interview.interviewer);
+        }
+      }
+    }
+  }
+  return interviewers;
 };
