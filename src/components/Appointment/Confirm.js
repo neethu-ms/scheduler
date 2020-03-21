606a9ef53cead3e5f30ecb73156a75ef21624632
neethu <msneethu2989@gmail.com>
2020-03-21 15:57:55 -0400

    Pass arguments for cancel and confirm

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -5,8 +5,8 @@ export default function(props){
 <main className="appointment__card appointment__card--confirm">
 <h1 className="text--semi-bold">{props.message}</h1>
   <section className="appointment__actions">
-    <Button danger onClick={props.onCancel("Cancelled")}>Cancel</Button>
-    <Button danger onClick={props.onConfirm("Confirmed")}>Confirm</Button>
+    <Button danger onClick={() => props.onCancel()}>Cancel</Button>
+    <Button danger onClick={() => props.onConfirm(props.id,props.interview)}>Confirm</Button>
   </section>
 </main>);
 };
