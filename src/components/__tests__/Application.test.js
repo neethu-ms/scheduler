edb3c524fd8c49f03274cf387ad0e275b5f6193b
neethu <msneethu2989@gmail.com>
2020-03-24 16:31:49 -0400

    Add test case for booking an appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,16 +1,34 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { getByText } from "@testing-library/react";
 
 import Application from "components/Application";
-
+/* import axios from "__mocks__/axios";
+jest.mock(axios); */
 
 afterEach(cleanup);
-
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
-  return waitForElement(() => getByText("Monday")).then(() => {
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"));
     fireEvent.click(getByText("Tuesday"));
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  })
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1",async () => {
+       const { container, debug } = render(<Application />);
+       await waitForElement(() => getByText(container,"Archie Cohen"));
+       const appointment = getAllByTestId(container, "appointment")[0];
+       fireEvent.click(getByAltText(appointment,"Add"));  
+      // debug();
+      fireEvent.change(getByPlaceholderText(appointment,/enter student name/i),{
+          target:{ value: "Lydia Miller-Jones"}
+       });
+       fireEvent.click(getByAltText(appointment,"Sylvia Palmer"));
+       fireEvent.click(getByText(appointment,"Save"));
+      console.log(prettyDOM(appointment));
+      expect(getByText(appointment, "Saving")).toBeInTheDocument();
+  });
+
 });
