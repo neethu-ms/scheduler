8863eb8a4c7e43de971c7c1d68f81963b05ec523
neethu <msneethu2989@gmail.com>
2020-03-25 17:22:24 -0400

    Add test cases for Error handling

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,22 +1,23 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText, queryByAltText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getAllByTestId, getByAltText, getByPlaceholderText, queryByText, queryByAltText, prettyDOM } from "@testing-library/react";
 import { getByText } from "@testing-library/react";
 
 import Application from "components/Application";
+import axios from "axios";
 
 afterEach(cleanup);
 describe("Application", () => {
-  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-    const { getByText } = render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText} = render(<Application />);
     await waitForElement(() => getByText("Monday"));
     fireEvent.click(getByText("Tuesday"));
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
+  }); 
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
      // 1. Render the Application.
-    const { container, debug } = render(<Application />);
+    const { container } = render(<Application />);
 
      // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -44,14 +45,15 @@ describe("Application", () => {
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
-
+   
     // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
-  });
+  }); 
 
   it("loads data, cancels an interview and increase the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
-    const { container,debug } = render(<Application />);
+    const { container} = render(<Application />);
+
   
     // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -60,7 +62,7 @@ describe("Application", () => {
          const appointment = getAllByTestId(container, "appointment").find(
            appointment => queryByText(appointment,"Archie Cohen")
          );
-
+        
          fireEvent.click(queryByAltText(appointment,"Delete"));
 
     // 4. Check that the confirmation message is shown.
@@ -79,14 +81,13 @@ describe("Application", () => {
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
-    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
-
-    debug();
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+   
   });
 
-  it("loads data, edits an interview and  spots remaining the same for Monday", async () => {
+   it("loads data, edits an interview and  spots remaining the same for Monday", async () => {
     // 1. Render the Application.
-    const { container,debug } = render(<Application />);
+    const { container } = render(<Application />);
   
     // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -120,7 +121,62 @@ describe("Application", () => {
       queryByText(day, "Monday")
     );
     expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+   
+  }); 
 
-    debug();
+ it("shows the save error when failing to save an appointment", async () => {
+  axios.put.mockRejectedValueOnce();
+   // 1. Render the Application.
+   const { container } = render(<Application />);
+
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+ 
+   //3. Click the "Add" button on the first empty appointment.
+  const appointment = getAllByTestId(container, "appointment")[0];
+  fireEvent.click(getByAltText(appointment, "Add"));
+
+     // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Lydia Miller-Jones" }
   });
+
+    // 5. Click the first interviewer in the list.
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+   // 6. Click the "Save" button on that same appointment.
+  fireEvent.click(getByText(appointment, "Save"));
+
+  // 7. Check that the element with the text "Could not create appointment" is displayed.
+  await  waitForElement(() => queryByText(appointment, "Could not create appointment"));
+  expect(getByText(appointment, "Could not create appointment")).toBeInTheDocument();
+ 
+}); 
+
+it("shows the delete error when failing to delete an existing appointment", async () => {
+  axios.delete.mockRejectedValueOnce();
+  // 1. Render the Application.
+  const { container } = render(<Application />);
+  
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  // 3. Click the "Delete" button on the booked appointment.
+       const appointment = getAllByTestId(container, "appointment").find(
+         appointment => queryByText(appointment,"Archie Cohen")
+       );
+
+       fireEvent.click(queryByAltText(appointment,"Delete"));
+
+  // 4. Check that the confirmation message is shown.
+  expect(getByText(appointment, "Are you sure you would like to delete")).toBeInTheDocument();
+
+  // 5. Click the "Confirm" button on the confirmation.
+  fireEvent.click(queryByText(appointment,"Confirm"));
+
+ // 6. Check that the element with the text "Could not create appointment" is displayed.
+ await waitForElement(() => queryByText(appointment, "Could not cancel appointment"));
+ expect(getByText(appointment, "Could not cancel appointment")).toBeInTheDocument();
+
+});  
 });
